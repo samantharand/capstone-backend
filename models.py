22518c31dcebd8543c0bc1c63ca147a146c5a622
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-05-16 18:41:06 -0500

    took out year

--- models.py
+++ models.py
@@ -26,7 +26,7 @@ class StreetArt(Model):
 	location = CharField()
 	latitude = FloatField() 
 	longitude = FloatField()
-	year = IntegerField()
+	# year = IntegerField()
 	artist = CharField()
 	description = TextField()
 	image = TextField()
